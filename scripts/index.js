1c5598093f86a3bd8f2da1dcf5d6950234727623
Vyacheslav <chealsey@gmail.com>
2024-07-13 14:55:38 +0400

    проектная работа номер 5

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content;
+const galleryPlace = document.querySelector(".places__list");
+
+function createCard(name, link, deleteCard) {
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector(".card__title").textContent = name;
+  card.querySelector(".card__image").src = link;
+  const deleteButton = card.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", deleteCard);
+
+  function deleteCard(event) {
+    event.target.closest(".card").remove();
+  }
+
+  return card;
+}
+
+initialCards.forEach((card) => {
+  const newCard = createCard(card.name, card.link, card.deleteCard);
+  galleryPlace.append(newCard);
+});
