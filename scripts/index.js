fb42437f67737499d5ce5ee9a87983be1f28e8c6
Vyacheslav <chealsey@gmail.com>
2024-07-13 18:21:44 +0400

    правки на пр5

--- scripts/index.js
+++ scripts/index.js
@@ -11,23 +11,29 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const galleryPlace = document.querySelector(".places__list");
 
-function createCard(name, link, deleteCard) {
+
+
+function createCard(name, link) {
   const card = cardTemplate.cloneNode(true);
 
   card.querySelector(".card__title").textContent = name;
   card.querySelector(".card__image").src = link;
+  card.querySelector(".card__image").setAttribute("alt","Фото с видом на " + name);
   const deleteButton = card.querySelector(".card__delete-button");
 
   deleteButton.addEventListener("click", deleteCard);
+
+ 
+
+  return card;
+}
 
   function deleteCard(event) {
     event.target.closest(".card").remove();
   }
 
-  return card;
-}
 
-initialCards.forEach((card) => {
-  const newCard = createCard(card.name, card.link, card.deleteCard);
-  galleryPlace.append(newCard);
+
+initialCards.forEach((card, deleteCard) => {
+  galleryPlace.append(createCard(card.name, card.link, deleteCard));
 });
