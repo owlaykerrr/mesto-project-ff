3208af1aa158e055e1362b0f72545918ad8759a1
Vyacheslav <chealsey@gmail.com>
2024-07-14 21:00:26 +0400

    правки аргумент функции в функции

--- scripts/index.js
+++ scripts/index.js
@@ -11,29 +11,25 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const galleryPlace = document.querySelector(".places__list");
 
-
-
-function createCard(name, link) {
+function createCard(name, link, deleteCard) {
   const card = cardTemplate.cloneNode(true);
 
   card.querySelector(".card__title").textContent = name;
   card.querySelector(".card__image").src = link;
-  card.querySelector(".card__image").setAttribute("alt","Фото с видом на " + name);
+  card
+    .querySelector(".card__image")
+    .setAttribute("alt", "Фото с видом на " + name);
   const deleteButton = card.querySelector(".card__delete-button");
 
   deleteButton.addEventListener("click", deleteCard);
 
- 
-
   return card;
 }
 
-  function deleteCard(event) {
-    event.target.closest(".card").remove();
-  }
+function deleteCard(event) {
+  event.target.closest(".card").remove();
+}
 
-
-
-initialCards.forEach((card, deleteCard) => {
+initialCards.forEach((card) => {
   galleryPlace.append(createCard(card.name, card.link, deleteCard));
 });
